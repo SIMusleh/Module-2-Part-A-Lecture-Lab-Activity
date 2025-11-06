6419f91f74dc504335f2f078a58609669eee90ce
SIMusleh <128906425+SIMusleh@users.noreply.github.com>
2025-11-06 23:38:23 +0000

    adding comments to snack.cpp and snack.h

--- snack.cpp
+++ snack.cpp
@@ -1,9 +1,11 @@
 #include "snack.h"
 #include <sstream>
 
+//Constructor for snack
 snack::snack(snackType type, int pn, std::string desc, double p, int q, bool isGlutenFree)
 : product(pn, desc, p, q), type(type), isGlutenFree(isGlutenFree) { }
 
+//Returns the snack type as a string
 std::string snack::getSnackType() const {
     switch (type) {
         case CHIPS: return "Chips";
@@ -12,19 +14,22 @@ std::string snack::getSnackType() const {
         default: return "Unknown";
     }
 }
-
+//Returns whether the snack is GF
 bool snack::getGlutenFree() const {
     return isGlutenFree;
 }
 
+//Sets the snack Type
 void snack::setSnackType(snackType t) {
     type = t;
 }
 
+//Sets GF Status
 void snack::setGlutenFree(bool gf) {
     isGlutenFree = gf;
 }
 
+//Returns a formatted string describing the snack
 std::string snack::tostring() const {
     std::ostringstream oss;
     oss << "Snack Product #" << prodNum << ": " << description
--- snack.h
+++ snack.h
@@ -6,6 +6,7 @@
 
 class snack : public product
 {
+    //Enum to represent different snack types
     public:
         enum snackType { CHIPS, COOKIES, NUTS };
 
@@ -14,11 +15,15 @@ class snack : public product
         bool isGlutenFree;
 
     public:
+
+        //constructor snack
         snack(snackType type, int pn, std::string desc, double p, int q, bool isGlutenFree);
         
+        //Accessors
         std::string getSnackType() const; 
         bool getGlutenFree() const;
-
+        
+        //Mutators
         void setSnackType (snackType t);
         void setGlutenFree(bool gf);
 
